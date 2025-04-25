805198d456c2eb01730013895c6dd545d3898ee6
carloshouser <carloshouser@gmail.com>
2025-04-25 15:57:40 -0300

    Aula 35. Carregando a imagem padrão de Perfil

--- meusite/routes.py
+++ meusite/routes.py
@@ -80,7 +80,9 @@ def sair():
 @app.route('/perfil')
 @login_required
 def perfil():
-    return render_template('perfil.html')
+    foto_perfil = url_for('static', filename=f'fotos_perfil/{current_user.foto_perfil}')
+    #foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('perfil.html', foto_perfil=foto_perfil)
 
 
 @app.route('/post/criacao')
